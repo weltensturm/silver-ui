19504a412314d022b0e0e217d1fd91e93dee0a3c
weltensturm <weltensturm@gmail.com>
2022-11-05 17:42:34 +0100

    better frameproxy error messages

--- lqt/frameproxy.lua
+++ lqt/frameproxy.lua
@@ -4,19 +4,25 @@ local LQT = namespace.LQT
 local FrameProxy = nil
 local FrameProxyMt = nil
 
+local get_context = function()
+    return strsplittable('\n', debugstack(3,0,1))[1]
+end
+
 FrameProxyMt = {
     __call = function(proxy, self, ...)
         if getmetatable(self) == FrameProxyMt then
             return FrameProxy {
                 __FrameProxy_parent = proxy,
                 __FrameProxy_action = 'callmethod',
-                __FrameProxy_args = { ... }
+                __FrameProxy_args = { ... },
+                __FrameProxy_context = get_context()
             }
         else
             return FrameProxy {
                 __FrameProxy_parent = proxy,
                 __FrameProxy_action = 'call',
-                __FrameProxy_args = { self, ... }
+                __FrameProxy_args = { self, ... },
+                __FrameProxy_context = get_context()
             }
         end
     end,
@@ -27,7 +33,8 @@ FrameProxyMt = {
         return FrameProxy {
             __FrameProxy_parent = self,
             __FrameProxy_action = 'index',
-            __FrameProxy_attr = attr
+            __FrameProxy_attr = attr,
+            __FrameProxy_context = get_context()
         }
     end,
     __tostring = function(self)
@@ -45,7 +52,7 @@ FrameProxyMt = {
 }
 
 FrameProxy = function(table)
-    return setmetatable(table or { __FrameProxy_parent=nil }, FrameProxyMt)
+    return setmetatable(table or { __FrameProxy_parent=nil, __FrameProxy_context=get_context() }, FrameProxyMt)
 end
 
 local function ApplyFrameProxy(frame, proxy)
@@ -55,12 +62,14 @@ local function ApplyFrameProxy(frame, proxy)
     else
         result = frame
     end
-    -- print(proxy)
     if proxy.__FrameProxy_action == 'index' then
+        assert(result ~= nil, 'Frame proxy returns nil at '..proxy.__FrameProxy_context)
         return result[proxy.__FrameProxy_attr], result
     elseif proxy.__FrameProxy_action == 'call' then
+        assert(result ~= nil, 'Frame proxy returns nil at '..proxy.__FrameProxy_context)
         return result(unpack(proxy.__FrameProxy_args))
     elseif proxy.__FrameProxy_action == 'callmethod' then
+        assert(result ~= nil, 'Frame proxy returns nil at '..proxy.__FrameProxy_context)
         return result(this, unpack(proxy.__FrameProxy_args))
     end
     return frame
